889e2fd9062314535e3265f6500f966ced9cc84c
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-03-29 16:24:44 +0000

    created devise users and user sample data

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  #get("/", { :controller => "stories", :action => "index"})
+  root to: "stories#index"
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get("/", { :controller => "stories", :action => "index"})
+
 
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,3 +1,15 @@
 desc "Fill the database tables with some sample data"
 task({ :sample_data => :environment }) do
+  User.destroy_all
+
+  usernames = ["admin", "user"]
+
+  usernames.each do |username|
+    user = User.new
+    user.email = "#{username}@example.com"
+    user.password = "password"
+    user.save
+  end
+
+  p "There are now #{User.count} users."
 end
