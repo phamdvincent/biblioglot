992bf1189db5e8d3a95c5d8b2dc8453c588b4d5b
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-04-01 23:12:51 +0000

    Added getting words from dictionary functionality

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,5 @@
 # require_relative "../services/audio"
-# require_relative "../services/dictionary"
+require_relative "../services/dictionary/dictionary_service"
 require_relative "../services/nlp/nlp_service"
 require_relative "../services/translation/translation_service"
 # require_relative "../services/storage"
@@ -21,18 +21,44 @@ class StoriesController < ApplicationController
   end
 
   def process_text
+    language = 'es'
     story_text = params[:story_text]
-    @processed_text = NLP_Service.get_nlp('es', story_text)
+    @processed_text = NLP_Service.get_nlp(language, story_text)
     @sentences = []
     @translations = []
+    @words = []
     @processed_text.each do |item|
-      @translations.append(Translation_Service.get_translation('es', item["sentence"]))
-      
+      # @translations.append(get_translation(language, item["sentence"])) # translations
+      # get_words_json(language, item["tokens"]) # words
+
     end
-    puts @translations
+    # puts @translations
+    puts @words
+    puts @words.class
     # @processed_text = "Hello World"
     # puts @processed_text
     # puts @processed_text.class
 
   end
+
+  private
+
+    def get_translation(language, sentence)
+      translated_sentence = Translation_Service.get_translation('es', sentence)
+      return translated_sentence
+    end
+
+    def get_words_json(language, tokens)
+
+      tokens.each do |token|
+        if token["upos"] != "PUNCT"
+          word = token["text"].downcase
+          word_json_list = Dictionary_Service.get_word_json_list(language, word)
+          word_hash = {"word": word, "json": word_json_list}
+          @words.append(word_hash)
+        end
+      end
+
+    end
+
 end
--- app/services/dictionary/dictionary_service.rb
+++ app/services/dictionary/dictionary_service.rb
@@ -1,43 +1,55 @@
 require "aws-sdk-dynamodb"
+require "json"
 
-class DynamoDBBasics
-  attr_reader :dynamo_resource
-  attr_reader :table
-
-  def initialize(table_name)
-    client = Aws::DynamoDB::Client.new(region: "us-east-2")
-    @dynamo_resource = Aws::DynamoDB::Resource.new(client: client)
-    @table = @dynamo_resource.table(table_name)
+module Dictionary_Service
+  class DynamoDBBasics
+    attr_reader :dynamo_resource
+    attr_reader :table
+  
+    def initialize(table_name)
+      client = Aws::DynamoDB::Client.new(region: "us-east-2")
+      @dynamo_resource = Aws::DynamoDB::Resource.new(client: client)
+      @table = @dynamo_resource.table(table_name)
+    end
+  
+    # Gets movie data from the table for a specific movie.
+    #
+    # @param title [String] The title of the movie.
+    # @param year [Integer] The release year of the movie.
+    # @return [Hash] The data about the requested movie.
+    def get_item(word)
+      @table.query({
+    index_name: 'word-index',
+    key_condition_expression: 'word = :word',
+    expression_attribute_values: {
+      ':word' => word
+    }
+      })
+    # @table.get_item({
+    #   key: {"uuid" => key}
+    # })
+    rescue Aws::DynamoDB::Errors::ServiceError => e
+      puts("Couldn't get word #{word} from table #{@table.name}:\n")
+      puts("\t#{e.code}: #{e.message}")
+      raise
+    end
+  
+  
   end
 
-  # Gets movie data from the table for a specific movie.
-  #
-  # @param title [String] The title of the movie.
-  # @param year [Integer] The release year of the movie.
-  # @return [Hash] The data about the requested movie.
-  def get_item(word)
-    @table.query({
-  index_name: 'word-index',
-  key_condition_expression: 'word = :word',
-  expression_attribute_values: {
-    ':word' => word
-  }
-    })
-  # @table.get_item({
-  #   key: {"uuid" => key}
-  # })
-  rescue Aws::DynamoDB::Errors::ServiceError => e
-    puts("Couldn't get word #{word} from table #{@table.name}:\n")
-    puts("\t#{e.code}: #{e.message}")
-    raise
+
+  def self.get_word_json_list(language, word)
+    if language == 'es'
+      dictionary = "spanish_dictionary"
+    end
+    db = DynamoDBBasics.new(dictionary)
+    parsed_json_list = []
+    json_list = db.get_item(word).items
+    json_list.each do |item|
+      json_object = JSON.parse(item["json"])
+      parsed_json_list.append(json_object)
+    end
+    return parsed_json_list
+
   end
 end
-
-def run
-  db = DynamoDBBasics.new("spanish_dictionary")
-  words = db.get_item("las")
-  puts words
-  puts words.class
-end
-
-run
