363e6950655ba331185bfae8cdceb5e8e4e014b7
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-04-30 13:49:27 +0000

    Refactored html into partials and deleted commented code

--- app/services/translation/translation.py
+++
@@ -1,6 +0,0 @@
-import deepl
-
-def get_translation(api_key, source_lang, content):
-  translator = deepl.Translator(api_key)
-  result = translator.translate_text(content, source_lang=source_lang, target_lang="EN-US")
-  return result.text
--- app/services/translation/translation_service.rb
+++ app/services/translation/translation_service.rb
@@ -1,17 +1,10 @@
 require 'json'
-# require 'pycall/import'
 
 require 'deepl'
 
 module Translation
   class TranslationService
     def self.get_translation(source_lang, content)
-      # PyCall.sys.path.append("./app/services/translation")
-      # # PyCall.sys.path.append('.')
-      # PyCall.import_module("translation")
-      # python_module = PyCall.import_module("translation")
-      # api_key = ENV["DEEPL_KEY"]
-      # response = python_module.get_translation(api_key, source_lang, content)
       DeepL.configure do |config|
         config.auth_key = ENV["DEEPL_KEY"]
       end
@@ -22,18 +15,3 @@ module Translation
     end
   end
 end
-
-# module TranslationService
-#   def self.get_translation(source_lang, content)
-#     PyCall.sys.path.append('./app/services/translation')
-#     # PyCall.sys.path.append('.')
-#     PyCall.import_module('translation')
-#     python_module = PyCall.import_module('translation')
-#     api_key = ENV["DEEPL_KEY"]
-#     response = python_module.get_translation(api_key, source_lang, content)
-#     return response
-
-#   end
-# end
-
-# # get_translation("ES", "SERÍAN las diez de la mañana de un día de octubre.")
